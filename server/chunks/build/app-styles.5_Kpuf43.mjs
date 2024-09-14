232d0b5efcdaf0366d2ada5bc6a6a7b2c9f5cf2d
Lerayaki <36731543+Lerayaki@users.noreply.github.com>
2024-09-14 12:56:58 +0200

    Publish nuxt app

--- server/chunks/build/app-styles.5_Kpuf43.mjs
+++
@@ -1,6 +0,0 @@
-import { a as app_vue_vue_type_style_index_0_lang } from './entry-styles-2.mjs-DWSZarS9.mjs';
-
-const appStyles_5_Kpuf43 = [app_vue_vue_type_style_index_0_lang, app_vue_vue_type_style_index_0_lang];
-
-export { appStyles_5_Kpuf43 as default };
-//# sourceMappingURL=app-styles.5_Kpuf43.mjs.map
